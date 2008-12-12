5d9f4b3bac57c497392f07da714339359901066a
zuwiki <zuwiki@gmail.com>
2008-12-11 17:09:12 -0800

    Generated User model. Plan for data models.

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,15 +1,9 @@
-# Don't forget that by default the salted_user mixin is used from merb-more
-# You'll need to setup your db as per the salted_user mixin, and you'll need
-# To use :password, and :password_confirmation when creating a user
-#
-# see merb/merb-auth/setup.rb to see how to disable the salted_user mixin
-# 
-# You will need to setup your database and create a user.
 class User
   include DataMapper::Resource
   
-  property :id,     Serial
-  property :login,  String
-  
-  
+  property :id, Serial
+  property :login, String
+
+  timestamps :on
+
 end
