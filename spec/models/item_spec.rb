b6241d6397be3537fc984f3079f16ce177a93515
zuwiki <zuwiki@gmail.com>
2008-12-19 20:42:28 -0800

    Tiny bit of work on item specs

--- spec/models/item_spec.rb
+++ spec/models/item_spec.rb
@@ -2,10 +2,20 @@ require File.join( File.dirname(__FILE__), '..', "spec_helper" )
 
 describe Item do
 
-  it "should have usage" do
+  it "should have integer usage" do
     i = Item.new
-    i.respond_to?(:usage).should == true
-    i.respond_to?(:usage=).should == true
+    i.usage = 4
+    i.usage.should == 4
+    i.valid?
+    i.errors.on(:usage).should be_nil
+  end
+
+  it "should have integer impact" do
+    i = Item.new
+    i.impact = 4
+    i.impact.should == 4
+    i.valid?
+    i.errors.on(:impact).should be_nil
   end
 
 end
