f2838489d5daa024a8b2c7c434ad0c60ce453806
zuwiki <zuwiki@gmail.com>
2008-12-26 21:13:44 -0800

    Making crypted_password and salt properties for users load lazily

--- config/init.rb
+++ config/init.rb
@@ -9,16 +9,22 @@ use_template_engine :erb
 Merb::Config.use do |c|
   c[:use_mutex] = false
   c[:session_store] = 'cookie'  # can also be 'memory', 'memcache', 'container', 'datamapper
-  
+
   # cookie session store configuration
-  c[:session_secret_key]  = '40e16e4e044c8e2520c322946ca83c91b868aaf7'  # required for cookie session store
-  c[:session_id_key] = '_lifestyle_session_id' # cookie session id key, defaults to "_session_id"
+  c[:session_secret_key]  = '40e16e4e044c8e2520c322946ca83c91b868aaf7'
+  c[:session_id_key] = '_lifestyle_session_id'
 end
  
 Merb::BootLoader.before_app_loads do
-  # This will get executed after dependencies have been loaded but before your app's classes have loaded.
+  Date.add_format(:pretty, "%A %B %d at %I:%M %p")
 end
  
 Merb::BootLoader.after_app_loads do
-  # This will get executed after your app's classes have been loaded.
+  # Redefine these attributes from the SaltedUser mixin to get laziness
+  # At some point I should really just reimpliment that junk and
+  # optimize it for OpenID and such. This is too hackish :\
+  User.class_eval do
+    property :crypted_password, String, :lazy => true
+    property :salt, String, :lazy => true
+  end
 end
