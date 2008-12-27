018056d07ce04bdc90a13f078aebdd1425f0062e
zuwiki <zuwiki@gmail.com>
2008-12-26 17:12:22 -0800

    basic user views

--- app/helpers/global_helpers.rb
+++ app/helpers/global_helpers.rb
@@ -1,5 +1,13 @@
 module Merb
   module GlobalHelpers
-    # helpers defined here available to all views.  
+    def time(datetime=DateTime.now, string="%A %B %d at %I:%M %p")
+      datetime.strftime(string)
+    end
+
+    def id(object, att=nil)
+      att = att ? "_" + att.to_s : ""
+      # TODO: really need to underscore classname
+      "#{object.class.to_s.downcase}_#{object.id}" + att
+    end
   end
 end
